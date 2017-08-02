0e04876dd0df9a4ec9ece8f7972a9f444b52da0b
George Miranda <george92miranda@gmail.com>
2017-08-02 00:03:20 -0700

    add form modal

--- src/app/footer/footer.component.ts
+++ src/app/footer/footer.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { FormBuilder, FormControl, FormGroup } from '@angular/forms';
 
 @Component({
   selector: 'app-footer',
@@ -7,13 +8,72 @@ import { Component, OnInit } from '@angular/core';
 })
 export class FooterComponent implements OnInit {
 
-  constructor() { }
+  stateFlags: Array<Boolean> = [false, false];
+  userForm: FormGroup;
+  tempEmail: string = "";
+  phoneNumber: FormControl;
 
-  ngOnInit() {
+  constructor(private formBuilder: FormBuilder) {
+    this.buildForm();
   }
 
-  emailModal() {
-    
+  ngOnInit() {
+    this.phoneNumber = this.userForm.get('phone') as FormControl;
+  }
+
+  private formatNumber(key) {
+    if (!isNaN(key)){
+      switch (this.phoneNumber.value.length) {
+        case 1:
+          if (this.phoneNumber.value[0] != '('){
+            this.phoneNumber.setValue("("+this.phoneNumber.value);
+          }
+          break;
+        case 5:
+          if (this.phoneNumber.value[4] != ')'){
+            this.phoneNumber.setValue(this.phoneNumber.value.substring(0,4) + ") " + this.phoneNumber.value[4]);
+          }
+          break;
+        case 6:
+          if (this.phoneNumber.value[5] != ' '){
+            this.phoneNumber.setValue(this.phoneNumber.value.substring(0,5) + " " + this.phoneNumber.value[5]);
+          }
+          break;
+        case 10:
+          if (this.phoneNumber.value[9] != '-'){
+            this.phoneNumber.setValue(this.phoneNumber.value.substring(0,9) + "-" + this.phoneNumber.value[9]);
+          }
+          break;
+        case 15:
+          this.phoneNumber.setValue(this.phoneNumber.value.substring(0,14));
+        default:
+          
+      }
+    } else {
+      this.phoneNumber.setValue(this.phoneNumber.value.substring(0,this.phoneNumber.value.length-1));
+    }
+  }
+
+  private send() {
+    console.log(this.userForm.value);
+    this.stateFlags[0] = false;
+  }
+
+  private toggleModal() {
+    if (this.tempEmail.length > 3){
+      let formEmail = this.userForm.get('email') as FormControl;
+      formEmail.setValue(this.tempEmail);
+      this.stateFlags[0] = true;
+    }
+  }
+
+  private buildForm() {
+    this.userForm = this.formBuilder.group({
+      name: this.formBuilder.control(null),
+      email: this.formBuilder.control(this.tempEmail),
+      phone: this.formBuilder.control(null),
+      msg: this.formBuilder.control(null)
+    });
   }
 
 }
